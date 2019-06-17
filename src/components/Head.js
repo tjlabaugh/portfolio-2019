01d7f731b6a8514d7bdc629f2ae1ffeb3cc20403
tjlabaugh <tjlabaugh@gmail.com>
2019-06-17 12:16:07 -0400

    Add favicon

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
+import favicon from '../images/favicon.png';
 
 const Head = ({ title }) => {
   const data = useStaticQuery(graphql`
@@ -16,6 +17,7 @@ const Head = ({ title }) => {
   return (
     <Helmet title={`${data.site.siteMetadata.title} | ${title}`}>
       <meta name="description" content="TJ LaBaugh Web Developer" />
+      <link rel="shortcut icon" type="image/png" href={favicon} />
     </Helmet>
   );
 };
